caaa101ad7944108f01849e57a5f88a8a52d401b
Kivasho <sebastianfsomellera@gmail.com>
2023-01-15 16:23:58 -0600

    Tabs

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,12 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import * as React from 'react'
-import { ChakraProvider, Heading, Stack, StackDivider } from '@chakra-ui/react'
+import { ChakraProvider, Heading, Stack, StackDivider, useColorModeValue } from '@chakra-ui/react'
 import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
 import { Text } from '@chakra-ui/react'
 import { Box } from '@chakra-ui/react'
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+
 
 import {
   Accordion,
@@ -15,34 +17,39 @@ import {
   AccordionPanel,
   AccordionIcon
 } from '@chakra-ui/react'
+
+
 export default function Home() {
   return (
+    
     <ChakraProvider>
-      <Card mt="10">
-        <CardHeader>
-          <Heading size="md">Client Report</Heading>
-        </CardHeader>
-        <CardBody>
-          <Stack divider={<StackDivider />} spacing='4'>
-            <Box>
-              <Heading size='xs' textTransform='uppercase'>
-                Summary
-              </Heading>
-              <Text pt='2' fontSize='sm'>
-                View a summary of your cients over the last month.
-              </Text>
-            </Box>
-            <Box>
-              <Heading size='xs' textTransform="uppercase">
-                Overview
-              </Heading>
-              <Text pt='2' fontSize='sm'>
-                Check out the overview of your clients.
-              </Text>
-            </Box>
-          </Stack>
-        </CardBody>
-      </Card>
+      <Box mt='10'>
+      <Tabs isFitted variant='enclosed'>
+        <TabList mb='1em'>
+          <Tab>
+            One
+          </Tab>
+          <Tab>
+            Two
+          </Tab>
+          <Tab>
+            Three
+          </Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <p>One!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>Two!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>Three!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+      </Box>
     </ChakraProvider>
+  
  )
 }
